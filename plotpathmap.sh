2e6c3c8af5c3e8355b0e6fe72fcf01909d0d1d40
David Stansby <dstansby@gmail.com>
2015-03-05 21:05:36 +0000

    If residuals no present, plot correctly without them

--- plotpathmap.sh
+++ plotpathmap.sh
@@ -21,24 +21,27 @@ makecpt -Cpolar -T-1.2/1.2/0.1 -Z > g.cpt
 #Plot coast
 pscoast  $proj -Bg45/g45:."": -Y1  -Dc -W0.0001p/210 -G220 $o >> map.ps
 
-#Put together path/station details and residual details
-paste -d " " stationdetails.dat filt/differences.dat wkbj/both/filt/differences.dat > temp.dat
+#If residual data is available, plot residuals at bottoming points
+if [ -f filt/differences.dat ] && [ -f wkbj/both/filt/differences.dat ]; then
+	paste -d " " stationdetails.dat filt/differences.dat wkbj/both/filt/differences.dat > temp.dat
+
+	#Plot bottoming points
+	awk '{ print $13, $12, $17-$20, 0.2,"c" }' temp.dat | psxy $proj  -Cg.cpt -S $o >> map.ps
+
+	#Plot scale + key
+	psscale -Cg.cpt -D12c/-0.5c/8c/0.6ch -X0.36 -B0.5:"Differential travel time residuals (s)": $o >>map.ps
+	rm temp.dat
+fi
 
 #Plot paths in the inner core
 for ((n=0; n <= $nmax ; n++))
 do
-        awk '{ if (NR == '$n') print $9 " " $8 " \n" $11 " " $10 }'  temp.dat  | psxy $proj -W0.01p/50 -K -O >> map.ps
+        awk '{ if (NR == '$n') print $9 " " $8 " \n" $11 " " $10 }'  stationdetails.dat  | psxy $proj -W0.01p/50 -K -O >> map.ps
 done
-
-#Plot bottoming points
-awk '{ print $13, $12, $17-$20, 0.2,"c" }' temp.dat | psxy $proj  -Cg.cpt -S $o >> map.ps
-
-#Plot scale + key
-psscale -Cg.cpt -D12c/-0.5c/8c/0.6ch -X0.36 -B0.5:"Differential travel time residuals (s)": $o >>map.ps
 
 #Finish up
 echo 0 0  | psxy -R1/2/1/2 -JX1/1 -Sp -O >> map.ps
-rm temp.dat g.cpt
+rm g.cpt
 
 #Open image
 gv map.ps &
