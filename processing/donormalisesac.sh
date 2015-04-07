90c3f6f9fd0058b355047faab42e34a546bd0840
David Stansby <dstansby@gmail.com>
2015-04-07 10:45:57 +0100

    Indent, remove -rf flags and delete temp files

--- processing/donormalisesac.sh
+++ processing/donormalisesac.sh
@@ -1,42 +1,40 @@
 for file in *fil
-
 do
 
-echo echo on > sacmac.m
-echo rh $file >> sacmac.m
-echo " sc echo &1,T2& > test" >> sacmac.m
-echo quit >> sacmac.m
+	echo echo on > sacmac.m
+	echo rh $file >> sacmac.m
+	echo " sc echo &1,T2& > test" >> sacmac.m
+	echo quit >> sacmac.m
 
-/usr/local/sac/bin/sac sacmac.m
-rm -fr sacmac.m
+	/usr/local/sac/bin/sac sacmac.m
+	rm sacmac.m
 
-start=`awk '{print $1-5}' test`
-end=`awk '{print $1+5}' test`
-echo $start $end
-rm -fr test
+	start=`awk '{print $1-5}' test`
+	end=`awk '{print $1+5}' test`
+	echo $start $end
+	rm test
 
-echo echo on > sacmac.m
-echo r $file >> sacmac.m
-echo cut $start $end >> sacmac.m
-echo r $file >> sacmac.m
-echo " sc echo $file >> test.dat " >> sacmac.m
-echo " sc echo &1,DEPMAX& &1,DEPMIN& >> test " >> sacmac.m
-echo quit >> sacmac.m
+	echo echo on > sacmac.m
+	echo r $file >> sacmac.m
+	echo cut $start $end >> sacmac.m
+	echo r $file >> sacmac.m
+	echo " sc echo $file >> test.dat " >> sacmac.m
+	echo " sc echo &1,DEPMAX& &1,DEPMIN& >> test " >> sacmac.m
+	echo quit >> sacmac.m
 
-/usr/local/sac/bin/sac sacmac.m
-rm -fr sacmac.m
+	/usr/local/sac/bin/sac sacmac.m
+	rm sacmac.m
 
-norm=`awk '{print ($1-$2)/2}' test`
-echo $norm
+	norm=`awk '{print ($1-$2)/2}' test`
+	echo $norm
 
-echo echo on > sacmac.m
-echo r $file >> sacmac.m
-echo div $norm >> sacmac.m
-echo w over >> sacmac.m
-echo quit >> sacmac.m
+	echo echo on > sacmac.m
+	echo r $file >> sacmac.m
+	echo div $norm >> sacmac.m
+	echo w over >> sacmac.m
+	echo quit >> sacmac.m
 
-/usr/local/sac/bin/sac sacmac.m
-rm -fr sacmac.m
-
+	/usr/local/sac/bin/sac sacmac.m
+	rm sacmac.m test test.dat
 
 done
